8aa0b5c530d0ded08c5446f72e54be30b8e2100c
Kristijan Maoduš <kmaodus@foi.hr>
2022-05-20 09:19:22 +0200

    Editing disabled

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import { Route, Routes } from "react-router-dom";
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
 import EmployeesList from "./components/employeesList";
-import Edit from "./components/edit";
 import Create from "./components/create";
 // import Footer from "./components/Footer";
  
@@ -16,7 +15,6 @@ const App = () => {
      <Navbar />
      <Routes>
        <Route exact path="/" element={<EmployeesList />} />
-       <Route path="/edit/:id" element={<Edit />} />
        <Route path="/create" element={<Create />} />
      </Routes>
      {/* <Footer /> */}
